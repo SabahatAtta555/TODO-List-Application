0c68c21b97ade3a67a02c9311fa9c6ac4bd632aa
SabahatAtta555 <163255976+SabahatAtta555@users.noreply.github.com>
2024-04-05 20:08:41 +0500

    TODO-List Application

---
+++ index.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.magenta.bold("\n\t Welcome to \'CodeWithSabi\' -TODO-LIST APPLICATION\n"));
+let todoList = [];
+let conditions = true;
+while (conditions) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "task",
+            type: "input",
+            message: chalk.green("Enter your New Task : ")
+        }
+    ]);
+    let latestTask = addTask.task;
+    todoList.push(latestTask);
+    console.log(latestTask + "  Task addad in TODO-List successfully");
+    let addMoreTask = await inquirer.prompt([
+        {
+            name: "addmore",
+            type: "confirm",
+            message: chalk.green("Do you want to add more task? "),
+            default: "False"
+        }
+    ]);
+    conditions = addMoreTask.addmore;
+}
+console.log("your updated TODO-List: ", todoList);
